e5a301ebc9fec0ce7670285de836ed8519142848
Mr. Wang Hong Wei <15536300457@163.com>
2023-03-08 10:06:17 +0800

    fix: 修改文件

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -5,39 +5,28 @@
  * eslint-plugin-prettier:将 prettier 的能力集成到 eslint 中。按照 prettier 的规则检查代码规范性，并进行修复。
  */
 module.exports = {
-	// 一行最多 120 字符..
-	printWidth: 120,
-	// 使用 2 个空格缩进
-	tabWidth: 2,
-	// 不使用缩进符，而使用空格
-	useTabs: false,
-	// 行尾需要有分号
-	semi: true,
-	// 使用单引号
-	singleQuote: true,
-	// 大括号内的首尾需要空格
-	bracketSpacing: true,
-	// jsx 标签的反尖括号需要换行
-	jsxBracketSameLine: false,
-	// 每个文件格式化的范围是文件的全部内容
-	rangeStart: 0,
-	rangeEnd: Infinity,
-	// 不需要写文件开头的 @prettier
-	requirePragma: false,
-	// 不需要自动在文件开头插入 @prettier
-	insertPragma: false,
-	// 使用默认的折行标准
-	proseWrap: 'preserve',
-	// 换行符使用 lf
-	endOfLine: 'lf',
-	quoteProps: 'as-needed', //对象的 key 仅在必要时用引号 "<as-needed|consistent|preserve>"引用对象中的属性时更改。
-	trailingComma: 'all', //"末尾需要有逗号 <es5|none|all>"在多行逗号分隔的句法结构中尽可能打印尾随逗号。
+	printWidth: 100, // 一行最多 120 字符..
+	semi: true, //行尾需要有分号
+	singleQuote: true, //使用单引号
+	tabWidth: 2, // 使用 2 个空格缩进
+	useTabs: true, //不使用缩进符，而使用空格 false
+	quoteProps: 'as-needed', //"对象的 key 仅在必要时用引号 <as-needed|consistent|preserve>"引用对象中的属性时更改。
+	trailingComma: 'all', //末尾需要有逗号 "<es5|none|all>"在多行逗号分隔的句法结构中尽可能打印尾随逗号。
+	bracketSpacing: true, //大括号内的首尾需要空格，在对象文字中的括号之间打印空格。
 	bracketSameLine: true, //将>多行 HTML（HTML、JSX、Vue、Angular）元素的 放在最后一行的末尾，而不是单独放在下一行（不适用于自关闭元素）。
-	arrowParens: 'avoid', //"箭头函数，只有一个参数的时候，也需要括号 <always|avoid>"在唯一的箭头函数参数周围包含括号。
-	htmlWhitespaceSensitivity: 'css', // 根据显示样式决定 html 要不要折行"<css|strict|ignore>"为 HTML、Vue、Angular 和 Handlebars 指定全局空白敏感度。
+	arrowParens: 'avoid', //"箭头函数，只有一个参数的时候，也需要括号，<always|avoid>"在唯一的箭头函数参数周围包含括号。
+	htmlWhitespaceSensitivity: 'strict', //"<css|strict|ignore>"为 HTML、Vue、Angular 和 Handlebars 指定全局空白敏感度。
 	vueIndentScriptAndStyle: true, //是否缩进Vue文件中的代码<script>和<style>标签。
+	endOfLine: 'lf', // 换行符使用 auto|lf
 	embeddedLanguageFormatting: 'auto',
-	requirePragma: false, //设置为true prettier自动格式化失效，必须在要格式化的文件头部添加vue:<!--  @prettier 、js:/***@prettier */等
+	requirePragma: false, //不需要写文件开头的 @prettier ,设置为true prettier自动格式化失效，必须在要格式化的文件头部添加vue:<!--  @prettier 、js:/***@prettier */等
+	insertPragma: false, // 不需要自动在文件开头插入 @prettier
+	jsxBracketSameLine: false, //jsx 标签的反尖括号需要换行
+	rangeStart: 0, // 每个文件格式化的范围是文件的全部内容
+	rangeEnd: Infinity,
+	proseWrap: 'preserve',
+	// 根据显示样式决定 html 要不要折行
+	htmlWhitespaceSensitivity: 'css',
 	overrides: [
 		{
 			files: '.prettierrc',
